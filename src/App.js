d545ee2d4a44d4828f9f5cb20b5134e6e446bb93
Poe <pwas123@gmail.com>
2022-06-27 21:44:41 -0400

    basic outlining done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import background from './images/sunsetForest'
 function App() {
+
+  function searchLocation(e){
+    console.log(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body' style={{backgroundImage: 'url(/sunsetForest.jpg)'}}>
+      <div className='top-content'>
+        <div className='weather-info'>
+          <div id='weatherType'>Overcast</div>
+          <div id='location'>Location</div>
+          <div id='date'>Date</div>
+          <div id='time'>Time</div>
+          <div id='temperature'>Temperature</div>
+          <div id='searchLocation'>
+            <input type='text' id='searchBoxInput' placeholder='Search Location...' 
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                searchLocation(e);
+              }
+            }}
+            />
+            <div id='searchButton'></div>
+          </div>
+        </div>
+        <div className='weather-details'>
+            <div id='feelsLike'>Feels like
+              
+            </div>
+            <div id='humidity'>Humidity</div>
+            <div id='rainChance'>Chance of Rain</div>
+            <div id='windSpeed'>Wind Speed</div>
+        </div>
+      </div>
     </div>
   );
 }
